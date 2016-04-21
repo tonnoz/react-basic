d4064304c53f99ceebeae9cf8140b4cf1bfd6ca7
Tonino Catapano <t.catapano@tmg.nl>
2016-04-21 09:46:59 +0200

    refactoring NodeList 2

--- app/components/Notes/NotesList.js
+++ app/components/Notes/NotesList.js
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 export class NotesList extends Component {
   render() {
+    { notes } = this.props; //destructuring this.props: take the notes property into the this.props and save it in a variable called notes
+    // so that I can take rid of the this.props into the <ul> and take it further by using the one line arrow function
     return (
       <ul className="list-group">
-        {
-          this.props.notes.map(function(note, index){
-            return <li className="list-group-item" key={index} > {note['.value']} </li>
-          })
-        }
+        {notes.map((note, index) => <li className="list-group-item" key={index} > {note['.value']} </li> )}
       </ul>
     )
   }
